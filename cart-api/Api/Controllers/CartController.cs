98b824b5a7ea437832cfa0a955742f338ec1f0ae
dmigo <d.f.goryunov@gmail.com>
2017-12-10 12:52:30 +0100

    Stub get endpoint

--- cart-api/Api/Controllers/CartController.cs
+++ cart-api/Api/Controllers/CartController.cs
@@ -3,21 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Api.Models;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     public class CartController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public CartController()
         {
-            return new string[] { "value1", "value2" };
+            
+        }
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new Cart{
+                Items = new []{
+                    new CartItem{
+                        Article = new ArticleShort{
+                            Id=1
+                        },
+                        Quantity = 10
+                    },
+                    new CartItem{
+                        Article = new ArticleShort{
+                            Id=32167
+                        },
+                        Quantity = 2
+                    },
+                    new CartItem{
+                        Article = new ArticleShort{
+                            Id=1337
+                        },
+                        Quantity = 17
+                    },
+                }
+            };
+
+            return Ok(result);
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put([FromBody]Cart cart)
         {
+            return NotFound();
         }
     }
 }
