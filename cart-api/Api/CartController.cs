84f250a9ce7a8c1b7112186409661b3b2d393e53
dmigo <d.f.goryunov@gmail.com>
2017-12-10 12:18:14 +0100

    Move project to Api directory

---
+++ cart-api/Api/CartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cart_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CartController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+    }
+}
