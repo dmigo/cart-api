a8cc01ffa42584e07d5688237954fed290ae99d7
dmigo <d.f.goryunov@gmail.com>
2017-12-10 12:43:27 +0100

    Bring namespaces in order

--- cart-api/Api/Program.cs
+++ cart-api/Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace cart_api
+namespace Api
 {
     public class Program
     {
--- cart-api/Api/Startup.cs
+++ cart-api/Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace cart_api
+namespace Api
 {
     public class Startup
     {
