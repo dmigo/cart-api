8f7a174ec396dbecb4eef9ca4baa248465d97306
dmigo <d.f.goryunov@gmail.com>
2017-12-12 21:54:07 +0100

    Split the files

---
+++ cart-api-client/Client/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Client
+{
+    //ToDo add comments
+    public class Article : IEquatable<Article>
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public bool Equals(Article other)
+        {
+            if (other == null)
+                return false;
+
+            if (this == other)
+                return true;
+
+            return GetHashCode() == other.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
---
+++ cart-api-client/Client/ShopClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client
+{
+    //ToDo add comments
+    public class ShopClient
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+
+        internal IDictionary<Article, int> FetchCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SubmitCart(IDictionary<Article, int> cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
