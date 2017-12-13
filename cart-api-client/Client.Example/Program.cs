b6b263a9ea19e477733cfd64a4386119ba7e8130
dmigo <d.f.goryunov@gmail.com>
2017-12-13 01:13:18 +0100

    Implement example application

---
+++ cart-api-client/Client.Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Client;
+
+namespace Client.Example
+{
+    class Program
+    {
+        static void PrintCart(Cart cart)
+        {
+            foreach (var keyValue in cart.Items)
+            {
+                Console.WriteLine($"{keyValue.Key.Id}: {keyValue.Value}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var host = "http://localhost";
+            var port = 5000;
+
+            var client = new ShopClient { Host = host, Port = port };
+            var cart = new Cart(client);
+
+            cart.Load();
+            Console.WriteLine("Loaded");
+            PrintCart(cart);
+            Console.WriteLine();
+
+            var article = new Article { Id = 1111 };
+            cart.Add(article, 10);
+            PrintCart(cart);
+            cart.Submit();
+            Console.WriteLine("Submitted");
+            Console.WriteLine();
+
+            cart.Add(article, 111);
+            PrintCart(cart);
+            cart.Submit();
+            Console.WriteLine("Submitted");
+            Console.WriteLine();
+
+            cart.Load();
+            Console.WriteLine("Loaded");
+            PrintCart(cart);
+        }
+    }
+}
